716fe100857ec9aa38882ad52933e9a2923b24a5
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-05-09 14:37:54 +0200

    [UPD] Add function app

--- src/App.jsx
+++ src/App.jsx
@@ -3,3 +3,14 @@ import { useState } from 'react'
 const avengers = ['IronMan', 'Spiderman', 'Capitan America', 'Hulk', 'Thor', 'Black Widow']
 
 
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1 className="my-3">Avengers</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
